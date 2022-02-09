21c43e59d61f656d66e49cb95415569a924a836e
amckernan1999 <44986722+amckernan1999@users.noreply.github.com>
2022-02-09 15:43:15 -0800

    Update main.js

--- Assignment#2/main.js
+++ Assignment#2/main.js
@@ -1,1 +1,24 @@
 
+/*Author: Andrew McKernan
+*CS385 HW 2
+*  2/9/22
+* Summary: initializes canvas, creates a cone object, and renders it
+*/
+ 
+function init() {
+    const canvas = document.getElementById("webgl-canvas");
+    const gl = canvas.getContext("webgl2");
+
+    function render() {
+        gl.clear(gl.COLOR_BUFFER_BIT);
+        cone.render();
+    }
+
+    gl.clearColor(0, 0, 0, .5);
+    let cone = new Cone(gl, 300);
+    render();
+}
+window.onload = init;
+
+
+
